99c8f6a8424d7c024cae95060018317af454d111
diart <diart@DESKTOP-GRU8VQP>
2024-11-17 15:14:53 +0100

    Changes to burger button

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.getElementById("main-nav");
     const burger = document.querySelector(".burger");
     const navLinks = document.querySelector(".nav-links");
+
+    let lastScrollY = window.scrollY;
+
+
+    nav.classList.add("compact");
+
+
+    window.addEventListener("scroll", () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY) {
+
+            nav.classList.add("hidden-down");
+            nav.classList.remove("hidden-up");
+        } else {
+
+            nav.classList.remove("hidden-down");
+            nav.classList.add("hidden-up");
+
+
+            if (currentScrollY === 0) {
+                nav.classList.remove("compact");
+            } else {
+                nav.classList.add("compact");
+            }
+        }
+
+        lastScrollY = currentScrollY;
+    });
+
 
     burger.addEventListener("click", () => {
         burger.classList.toggle("active");
         navLinks.classList.toggle("active");
+
+
+        if (navLinks.classList.contains("active")) {
+            nav.classList.remove("compact");
+        } else {
+            nav.classList.add("compact");
+        }
     });
 });
