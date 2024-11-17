c80a1324107348221e2c1572ccdc17d185912b60
diart <diart@DESKTOP-GRU8VQP>
2024-11-17 09:08:07 +0100

    Bad feedback from teacher, rollback

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const sideMenu = document.querySelector('.side-menu');
-
-    menuIcon.addEventListener('click', function() {
-        sideMenu.classList.toggle('active'); // Переключаем класс 'active'
-    });
-});
